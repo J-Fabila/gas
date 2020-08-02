188b2821546362682c4d930cec5080972c6a03d5
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-08-02 17:43:16 -0500

    Update gas.py

--- gas.py
+++ gas.py
@@ -19,86 +19,6 @@ lennard_jones={'H' : [43,3.3] , 'O' : [162,3.9], 'N' : [131,4.2] } #lenard_jones
 #                                         Declaración de clases
 ## # # # # # #  #  #  #   #   #     #    #    #    #   #  #  ##
 """
-self.e = lennard_jones[Simbolo][0]              # Energía disociación [J]
-self.r = lennard_jones[Simbolo][1]              # Radio de equilibrio [A]
-"""
-
-class Atomo:
-    def __init__(self, _Simbolo, _x, _y, _z):
-        self.Simbolo = _Simbolo                          # Simbolo atómico
-        self.R = Radios[_Simbolo]               # Radio atómico [A]
-        self.M =  Masas[_Simbolo] #*1.66054e-27    # Masa atómica [uma]
-        self.x = _x  # Posición X [A]
-        self.y = _y  # Posición Y [A]
-        self.z = _z  # Posición Z [A]
-#### ALTERNATIVA: self.x = np.zeros(3) self.x[0] == X ; self.x[1] == Y ;self.x[1] == Z
-        self.vx = _v_x                                     # Velocidad X [A/s]
-        self.vy = _v_y                                     # Velocidad X [A/s]
-        self.vz = _v_z                                     # Velocidad X [A/s]
-#### ALTERNATIVA: self.v = np.zeros(3) self.v[0] == X ; self.v[1] == Y ;self.v[1] == Z
-        self.ax = 0                                     # Aceleración X [A/s^2]
-        self.ay = 0                                     # Aceleración Y [A/s^2]
-        self.az = 0                                     # Aceleración Z [A/s^2]
-#### ALTERNATIVA: self.a = np.zeros(3) self.a[0] == X ; self.a[1] == Y ;self.a[1] == Z
-    def __repr__(self):
-        return self.Symbol+" "+str(self.x)+" "+str(self.y)+" "+str(self.z)+"\n"
-
-
-class Estructura_Atomica:
-    def __init__(self, N):
-        self.Nat=N   # Número de átomos
-        self.atomos=[]
-    def Initializacion_Boltzmann(self, Temperatura, Celda):
-        #Implementar inicializacion de velocidades
-        #v=np.sqrt(np.sqrt(8.0*k*T/(np.pi*(self.m))))/3.0 # Velocidad de acuerdo con Boltzmann
-        pass
-    def Leer_XYZ(self, file):
-        with open(file,"r") as f:
-            data=f.readlines()
-            contador=1
-            for line in data:
-                words = line.split()
-                if(contador==1):
-                    self.Nat=words[0]
-                if(contador>2):
-                    atom=Atomo(words[0],words[1],words[2],words[3])
-                    self.atomos.append(atom)
-                contador=contador+1
-        print(self.atoms)
-
-class celda:
-    def __init__(self,xmin=-10.0,xmax=10.0,ymin=-10.0,ymax=10.0,zmin=-10.0,zmax=10.0):
-        self.xmin=xmin
-        self.xmax=xmax
-        self.ymin=ymin
-        self.ymax=ymax
-        self.zmin=zmin
-        self.zmax=zmax
-
-## # # # # # #  #  #  #   #   #     #    #    #    #   #  #  ##
-#                                      Declaración de funciones
-## # # # # # #  #  #  #   #   #     #    #    #    #   #  #  ##
-
-def distancia_atomica(atom_1,atom_2):
-    return np.sqrt( np.power(atom_1.vx-atom_2.vx,2)+np.power(atom_1.vy-atom_2.vy,2)+np.power(atom_1.vz-atom_2.vz,2))
-## A lo mejor habria k vectorizarla
-
-def Fuerza_l_j(atom_1,atom_2):
-    r_sep=distancia_atomica(atom_1,atom_2)
-    return 4*atom_1.e*( (12.0*np.power((atom_1.r),12)/np.power(r_sep,13)) - (6.0*np.power((atom_1.r),6)/np.power(r_sep,7)) )
-## A lo mejor tambien habria k vectorizarla
-"""''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-Otra opción es implementar las fuerzas como vectores (devuelvan vectores)
-,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"""
-
-for i in range(N_particulas):
-    gas.append(atomo('H',caja))
-
-while criterio_tiempo==0:
-    for i in range(N_particulas):  ##Calcula las fuerzas para todas las particulas
-        fx=0 ; fy=0 ; fz=0 ;
-        for j in range(N_particulas):
-            if (i!=j):
                 F=l_j(gas[i],gas[j])
                 fx=fx+F*(gas[i].x-gas[j].x)
                 fy=fy+F*(gas[i].y-gas[j].y)
@@ -119,8 +39,125 @@ while criterio_tiempo==0:
     tiempo=tiempo+1;
     if(tiempo>tiempo_total):
         criterio_tiempo=1;
+"""
 
+"""
+self.e = lennard_jones[Simbolo][0]              # Energía disociación [J]
+self.r = lennard_jones[Simbolo][1]              # Radio de equilibrio [A]
+"""
+
+class Atomo:
+    def __init__(self, _Simbolo, _x, _y, _z):
+        self.Simbolo = _Simbolo                          # Simbolo atómico
+        self.R = Radios[_Simbolo]               # Radio atómico [A]
+        self.M =  Masas[_Simbolo] #*1.66054e-27    # Masa atómica [uma]
+        self.x = np.zeros(3) # self.x[0] == X ; self.x[1] == Y ;self.x[1] == Z
+        self.v = np.zeros(3) #self.v[0] == X ; self.v[1] == Y ;self.v[1] == Z
+        self.a = np.zeros(3) #self.a[0] == X ; self.a[1] == Y ;self.a[1] == Z
+    def __repr__(self):
+        return self.Symbol+" "+str(self.x)+" "+str(self.y)+" "+str(self.z)+"\n"
+
+
+class Estructura_Atomica:
+    def __init__(self, N):
+        self.Nat=N   # Número de átomos
+        self.atomo=[]
+    def Initializacion_Posiciones(self, Celda):
+        # distribucion aleatoria de posiciones dentro de la Celda
+        pass
+    def Initializacion_Boltzmann(self, Temperatura, celda):
+        #Implementar inicializacion de velocidades
+        #v=np.sqrt(np.sqrt(8.0*k*T/(np.pi*(self.m))))/3.0 # Velocidad de acuerdo con Boltzmann
+        pass
+    def Leer_XYZ(self, file):
+        with open(file,"r") as f:
+            data=f.readlines()
+            contador=1
+            for line in data:
+                words = line.split()
+                if(contador==1):
+                    self.Nat=words[0]
+                if(contador>2):
+                    atom=Atomo(words[0],words[1],words[2],words[3])
+                    self.atomo.append(atom)
+                contador=contador+1
+        print(self.atoms)
+    def Imprimir_XYZ(self, file):
+        #Escriba XYZ?
+        pass
+
+class Celda:
+    def __init__(self,xmin=-10.0,xmax=10.0,ymin=-10.0,ymax=10.0,zmin=-10.0,zmax=10.0), periodicidad_=True, radio):
+        self.xmin=xmin
+        self.xmax=xmax
+        self.ymin=ymin
+        self.ymax=ymax
+        self.zmin=zmin
+        self.zmax=zmax
+        self.periodicidad=periodicidad_
+        self.radio_cutoff=radio
+
+## # # # # # #  #  #  #   #   #     #    #    #    #   #  #  ##
+#                                      Declaración de funciones
+## # # # # # #  #  #  #   #   #     #    #    #    #   #  #  ##
+
+def distancia_atomica(atom_1,atom_2):
+    return np.sqrt( np.power(atom_1.vx-atom_2.vx,2)+np.power(atom_1.vy-atom_2.vy,2)+np.power(atom_1.vz-atom_2.vz,2))
+## A lo mejor habria k vectorizarla
+
+def Fuerza_l_j(atom_1,atom_2):
+    r_sep=distancia_atomica(atom_1,atom_2)
+    return 4*atom_1.e*( (12.0*np.power((atom_1.r),12)/np.power(r_sep,13)) - (6.0*np.power((atom_1.r),6)/np.power(r_sep,7)) )
+## A lo mejor tambien habria k vectorizarla
+"""''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Otra opción es implementar las fuerzas como vectores (devuelvan vectores)
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"""
+
+def Dinamica_Molecular(e_a,caja, Fuerza_Externa=False ,n_max=1000):
+    """ e_a === objeto de Estructura_Atomica """
+    F=np.zeros(e_a.Nat, e_a.Nat)
+    Aux=np.zeros(e_a.Nat,3)
+    while (paso < n_max):
+        # Actualiza las posiciones
+        for i in range(e_a.Nat):
+           e_a.atomo[i].x=e_a.atomo[i].x+e_a.atomo[i].v*deltaT+(e_a.atomo[i].a*deltaT**2)/2
+            if(caja.periodicidad==True):
+                #Implementar reflexiones
+                pass
+        # Calcula las fuerzas de LJ
+        for i in range(e_a.Nat):
+            for  j in range(i,e_a.Nat):
+                if(caja.periodicidad==False):
+                    distancia=distancia_atomica(e_a.atomo[i], atomo[j])
+                else:
+                    #condiciones PERIODICAS
+                    pass
+                if(distancia<caja.radio_cutoff):
+                    F[i,j]=Fuerza_l_j(atom[i],atom[j])
+                    F[j,i]=F[i,j]
+                else
+                    F[i.j]=0
+                    F[j,i]=0
+        #Calcula las aceleraciones totales siguiente paso
+        for i in range(e_a.Nat):
+            Aux[i,:]=0
+            for j in range(e_a.Nat):
+                if (i!=j):
+                    Aux[i,:]=Aux[i,:]+(F[i,j]/e_a.atomo[i].M)*(e_a.atomo[i].x-e_a.atomo[j].x)
+            if(Fuerza_Externa!=False):
+                Aux[i,:]=Aux[i,:]+(Fuerza_Externa(e_a.atomo[i]))/e_a.atomo[i].M
+        #Calcula las nuevas velocidades
+        for i in range(e_a.Nat):
+            e_a.atomo[i].v=e_a.atomo[i].v+(Aux[i,:]+e_a.atomo[i].a)*deltaT/2.0
+        #Intercambia las viejas aceleraciones por las nuevas
+        for i in range(e_a.Nat):
+            e_a.atomo[i].a[i,:]=Aux[i,:]
+        e_a.Imprimir_XYZ("dinamica.xyz"+str(paso))
+        #ACA VAN LOS LETREROS
+        print(" step i/N etc ... ")
+########################################################################
 caja=celda()
 gas=Estructura_Atomica()
+gas.Initializacion_Posiciones(caja)
 gas.Initializacion_Boltzmann()
-Dinamica_molecular(gas,celda)
+Dinamica_Molecular(gas,celda)
